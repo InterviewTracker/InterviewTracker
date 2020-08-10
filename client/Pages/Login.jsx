f3dc80f78a03e2a541d0b2581dff84dd441a194a
JIMvsTHEWORLD <JMArmbruster@gmail.com>
2020-08-10 16:18:53 -0400

    added login and register forms

--- client/Pages/Login.jsx
+++ client/Pages/Login.jsx
@@ -11,7 +11,79 @@ class Login extends React.Component {
   }
   render(){
     return(
+      <div className='MainDiv'>
+        <div className='Login'>
+          <form className='LoginForm'>
+              <h4>Login</h4>
+              <label>Username:  </label>
+              <input
+                id='username'
+                type='text'
+                name='username'
+                placeholder='Username'
+              />
+              <br />
+              <label>Password:  </label>
+              <input
+                id='password'
+                type='password'
+                name='password'
+                placeholder='Password'
+              />
+              <br />
+              <button type='submit' id='loginButton'>Login</button>
+          </form>
+        </div>
+        <div className="OAuth">
 
+        </div>
+        <div className='Register'>
+          <form className='RegisterForm'>
+            <hr>Register</hr>
+            <label>Username:  </label>
+            <input 
+              id='newUsername'
+              type='text'
+              name='newUsername'
+              placeholder='Username'
+            />
+            <br />
+            <label>Email:  </label>
+            <input
+              id='email'
+              type='text'
+              name='email'
+              placeholder='Email'
+            />
+            <br />
+            <label>GitHub Username (Optional):  </label>
+            <input
+              id='githubName'
+              type='text'
+              name='githubName'
+              placeholder='GitHub Username'
+            />
+            <br />
+            <label>Password:  </label>
+            <input
+              id='newPassword'
+              type='password'
+              name='newPassword'
+              placeholder='Password'
+            />
+            <br />
+            <label>Re-Enter Password:  </label>
+            <input
+              id='reEnterPassword'
+              type='password'
+              name='password'
+              placeholder='Password'
+            />
+            <br />
+            <button type='submit' id='registerButton'>Sign Up</button>
+          </form>
+        </div>
+      </div>
     )
   }
 }
