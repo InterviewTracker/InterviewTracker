1f779a49e5b3bfc7be657c53a41e6008cdbceec9
JIMvsTHEWORLD <JMArmbruster@gmail.com>
2020-08-10 16:01:07 -0400

    boiler plate code for Login

--- client/App.jsx
+++ client/App.jsx
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-
-class App extends React.Component{
-    
-
-    render(){
-    return(
-        <div>
-            
-        </div>
-    )
-  }
-}
-=======
 import Login from './Pages/Login.jsx';
 import Profile from './Pages/Profile.jsx';
 import Feed from './Pages/Feed.jsx';
@@ -30,4 +16,3 @@ class App extends React.Component {
 
 ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
->>>>>>> 1c995f33cdd65dffe07915591992ada7d761df66
---
+++ client/Pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import '../styles/styles.scss'
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default Login
