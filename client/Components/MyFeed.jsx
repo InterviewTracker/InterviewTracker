9633e0f36a39ce5fb6a5913abda37e7b77660334
Nicole Ip <nicoleapril29@gmail.com>
2020-08-13 09:08:48 -0500

    Added styles to MyFeed and edited questions

--- client/Components/MyFeed.jsx
+++ client/Components/MyFeed.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
-import '../styles/styles.scss'
+import React, { useState, useEffect } from "react";
+import Interview from "../Pages/Feed/Interview.jsx";
+import "../styles/styles.scss";
 
-
-function MyFeed () {
-    return (
-      <div id="myFeed">
-          <h2>My Interviews</h2>
-       
-      </div>
-  );
+function MyFeed() {
+  const [interviewsFromDb, setInterviews] = useState([]);
+  useEffect(() => {
+    fetch("/user/userFeed")
+      .then((data) => data.json())
+      .then((res) => {
+        console.log("res", res);
+        setInterviews([...interviewsFromDb, ...res]);
+      });
+  }, []);
+  // const data = [
+  //   { id: 1, company: 'google' },
+  //   { id: 2, company: 'amazon' },
+  //   { id: 3, company: 'kmart' },
+  // ];
+  const interviews = [];
+  if (interviewsFromDb !== []) {
+    let i = 0;
+    console.log("interviews from db", interviewsFromDb);
+    while (i < interviewsFromDb.length) {
+      interviews.push(<Interview key={i} interviews={interviewsFromDb[i]} />);
+      i++;
+    }
+  }
+  return <div>{interviews}</div>;
 }
 
 export default MyFeed;
--- server/routers/contentRouter.js
+++ server/routers/contentRouter.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const contentController = require('../controllers/contentController.js')
+const contentController = require("../controllers/contentController.js");
 
-router.post('/', contentController.filterContent, (req, res, next) => {
-  res.status(200).json(res.body)
+router.post("/", contentController.filterContent, (req, res, next) => {
+  res.status(200).json(res.body);
 });
 
-router.post('/addContent', contentController.addContent, (req, res, next) => {
-  res.status(200).json(res.body)
+router.get("/allContent", contentController.getContent, (req, res, next) => {
+  res.status(200).json(res.body);
 });
 
-router.post('/editContent', contentController.editContent, (req, res, next) => {
-  res.status(200).json(res.body)
+router.post("/addContent", contentController.addContent, (req, res, next) => {
+  res.status(200).json(res.body);
 });
 
-router.delete('/deleteContent', contentController.deleteContent, (req, res, next) => {
-  res.status(200).json(res.body)
+router.post("/editContent", contentController.editContent, (req, res, next) => {
+  res.status(200).json(res.body);
 });
 
+router.delete(
+  "/deleteContent",
+  contentController.deleteContent,
+  (req, res, next) => {
+    res.status(200).json(res.body);
+  }
+);
 
 module.exports = router;
